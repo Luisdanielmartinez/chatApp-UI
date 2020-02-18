168e2e48e834bd263bd36585ac2db5fc6a130537
luis d martinez <luis d martinez@FYRMN32>
2020-02-18 16:47:42 -0400

    fix the resources in inboxPage

--- ChatApp/ChatApp/Models/Conversation.cs
+++ ChatApp/ChatApp/Models/Conversation.cs
@@ -28,7 +28,7 @@ namespace ChatApp.Models
             new Conversation{Subject="Accepted - Jessie",Body="classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock",Category="Modern Beach House",IsAccepted=true, UserProfilePicture="https://randomuser.me/api/portraits/men/43.jpg"},
             new Conversation{Subject="Expired - Duane",Body="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",Category="Beautiful & Elegant Mansion",IsAccepted=false, UserProfilePicture="https://randomuser.me/api/portraits/men/29.jpg"},
             new Conversation{Subject="Expired - Anthony",Body=" aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",Category="Beautiful & Elegant Mansion",IsAccepted=false, UserProfilePicture="https://randomuser.me/api/portraits/women/70.jpg"},
-            new Conversation{Subject="Expired - Anthony",Body=" aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",Category="Beautiful & Elegant Mansion",IsAccepted=false, UserProfilePicture="https://randomuser.me/api/portraits/women/70.jpg"}
+            new Conversation{Subject="Expired - Anthony",Body=" aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",Category="Beautiful & Elegant Mansion",IsAccepted=true, UserProfilePicture="https://randomuser.me/api/portraits/women/70.jpg"}
         };
     }
 }
