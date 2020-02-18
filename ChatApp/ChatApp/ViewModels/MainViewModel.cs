5784f79415fc6cdaf123550b491395764ec59498
luis d martinez <luis d martinez@FYRMN32>
2020-02-18 15:23:16 -0400

    initial commit

---
+++ ChatApp/ChatApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ChatApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ChatApp.ViewModels
+{
+   public class MainViewModel:INotifyPropertyChanged
+    {
+        public List<Conversation> Messages { get; set; } = Conversations.All;
+
+        public MainViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ ChatApp/ChatApp/Views/InBoxPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ChatApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InBoxPage : ContentPage
+    {
+        public InBoxPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
