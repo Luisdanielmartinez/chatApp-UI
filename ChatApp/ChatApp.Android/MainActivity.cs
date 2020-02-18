6a3a98f53f50c50b4a0d5dfd7a7bd39919d056f8
luis d martinez <luis d martinez@FYRMN32>
2020-02-18 15:31:09 -0400

    add the collection view

--- ChatApp/ChatApp.Android/MainActivity.cs
+++ ChatApp/ChatApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace ChatApp.Droid
 {
@@ -20,6 +21,7 @@ namespace ChatApp.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Forms.SetFlags("CollectionView_Experimental");
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
