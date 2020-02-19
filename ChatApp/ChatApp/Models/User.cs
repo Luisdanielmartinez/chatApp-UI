d7c48e73186dfb33a79a5e52067a21f4e318a7c4
Luis Daniel <Luis Daniel@DESKTOP-RK2RH1T>
2020-02-18 20:34:34 -0500

    add the class user and chats

---
+++ ChatApp/ChatApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.Models
+{
+   public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Tell { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PhotoPerfile { get; set; }
+    }
+}
